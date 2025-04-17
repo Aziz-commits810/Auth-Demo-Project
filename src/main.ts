601b09a232626f457eaafac0e15202e477631258
Aziz ur Rehman <aziz.rehman@argonteq.com>
2025-04-17 19:39:11 +0500

    cors configuration

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    credentials: true,
+  });
+
   // Global filters
   app.useGlobalFilters(new HttpExceptionFilter());
 
